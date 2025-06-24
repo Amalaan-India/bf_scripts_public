5c2072b53bd0af4566d3e3141bbcd32836668530
arvindbugflows <arvind@bugflows.com>
2025-06-24 14:51:36 +0530

    Update backend_non_prod_startup_script_01.sh

--- backend_non_prod_startup_script_01.sh
+++ backend_non_prod_startup_script_01.sh
@@ -33,22 +33,10 @@ curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-comp
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 sudo systemctl restart docker
 
-logF "Installing Rust"
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh #Rust https://doc.rust-lang.org/book/ch01-01-installation.html#installing-rustup-on-linux-or-macos
-chmod +r "$HOME/.cargo/env"
-. "$HOME/.cargo/env"
-
 logF "Enabling passwordless sudo for current user"
 echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER
 sudo chmod 0440 /etc/sudoers.d/$USER
 
-logF "Enabling password based SSH"
-sed -i '/^PasswordAuthentication/d' /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
-
-logF "Starting SSH and enabling autostart on boot"
-sudo systemctl enable ssh
-sudo systemctl start ssh
 
 logF "Updating and Upgrading"
 sudo apt-get update -y
